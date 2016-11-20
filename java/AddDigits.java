f85d8c80e4409ad7796fc8ce748187899d50977a
Luke Chang <luke.x.chang@gmail.com>
2016-11-20 19:08:57 +1300

    All must frequent questions from LeetCode

---
+++ java/AddDigits.java
@@ -0,0 +1,10 @@
+public class AddDigits {
+	// LeetCode 258 Add digits
+	public static int addDigits(int num) {
+		return (num - 1) % 9 + 1;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
